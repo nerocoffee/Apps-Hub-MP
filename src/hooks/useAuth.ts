6170d4998c14ddccdb387568150c0ff8b5344a9d
Thitiphan Phiphatthaloengkun <nerocoffee.com@gmail.com>
2026-01-11 20:50:42 +0700

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase, Profile } from '../lib/supabase'
+import { supabase, Profile } from '../lib/supabase.ts'
 import { User, Session } from '@supabase/supabase-js'
 
 export function useAuth() {
